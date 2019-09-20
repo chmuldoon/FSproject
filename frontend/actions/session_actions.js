ac5fc27a6d180cea7073b670d0c0d7093eda5f67
Charles Muldoon <charm3728@gmail.com>
2019-09-20 12:03:52 -0700

    fixed receiveSessionErrors

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -26,7 +26,7 @@ export const signup = user => dispatch => (
   APIUtil.signup(user).then(user => {
     dispatch(receiveCurrentUser(user))
   }, err => (
-    dispatch(receiveErrors(err.responseJSON))
+    dispatch(receiveSessionErrors(err.responseJSON))
   ))
 );
 
@@ -34,7 +34,7 @@ export const login = user => dispatch => (
   APIUtil.login(user).then(user => (
     dispatch(receiveCurrentUser(user))
   ), err => (
-    dispatch(receiveErrors(err.responseJSON))
+    dispatch(receiveSessionErrors(err.responseJSON))
   ))
 
 );
